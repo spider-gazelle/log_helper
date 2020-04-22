fc0aa08621825f1e6eb8c17525c467583beb3b74
Caspian Baska <caspianbaska@gmail.com>
2020-04-22 23:11:02 +1000

    fix: remove explicit block typing

--- src/log_helper.cr
+++ src/log_helper.cr
@@ -11,16 +11,19 @@ class Log
   }
 
   {% for method, severity in SEVERITY_MAP %}
-  def {{method.id}}(*, exception : Exception? = nil, &block : -> NamedTuple | Hash(String | Symbol, V)) forall V
+  def {{method.id}}(*, exception : Exception? = nil)
     Log.with_context do
       previous_def(exception: exception) do
         block_result = yield
-        block_result = block_result.to_h if block_result.is_a? NamedTuple && block_result.has_key?("message")
 
-        message = block_result.delete(:message) || block_result.delete("message") if block_result.is_a? Hash
-        Log.context.set(block_result)
-
-        message
+        if block_result.is_a? NamedTuple || block_result.is_a? Hash
+          block_result = block_result.to_h if block_result.is_a? NamedTuple && block_result.has_key?("message")
+          message = block_result.delete(:message) || block_result.delete("message") if block_result.is_a? Hash
+          Log.context.set(block_result)
+          message
+        else
+          block_result
+        end
       end
     end
   end
