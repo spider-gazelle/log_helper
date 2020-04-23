a5b40c74579054e7fd4047c1849827ae37100ebd
Stephen von Takach <steve@acaprojects.com>
2020-04-23 17:44:45 +1000

    feat: optimise performance

--- src/log_helper.cr
+++ src/log_helper.cr
@@ -12,20 +12,20 @@ class Log
 
   {% for method, severity in SEVERITY_MAP %}
   def {{method.id}}(*, exception : Exception? = nil)
-    Log.with_context do
-      previous_def(exception: exception) do
-        block_result = yield
+    block_result = yield
+    block_result = block_result.to_h if block_result.is_a? NamedTuple
 
-        block_result = block_result.to_h if block_result.is_a? NamedTuple
-
-        if block_result.is_a? Hash
+    # Only call with_context if the block is a hash
+    if block_result.is_a? Hash
+      Log.with_context do
+        previous_def(exception: exception) do
           message = block_result.delete(:message) || block_result.delete("message")
           Log.context.set(block_result)
           message
-        else
-          block_result
         end
       end
+    else
+      previous_def(exception: exception) { block_result }
     end
   end
   {% end %}
